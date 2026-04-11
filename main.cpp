2443d2be3da262d469d067aa2c19a67f7de660cb
SeungBeom Kim@Desktop <64590456+wewe231223@users.noreply.github.com>
2026-04-11 12:44:33 +0900

    한글 출력

--- main.cpp
+++ main.cpp
@@ -1,69 +1,74 @@
+﻿#include <iostream>
+#include <locale>
 #include "Renderer/Renderer.h"
 
-#include <iostream>
 
-#ifdef DEBUG
+#ifdef _DEBUG
 #pragma comment(lib, "debug/glfw3.lib")
 #else
 #pragma comment(lib, "release/glfw3.lib")
 #endif
 
 int main() {
-    std::cout << "가나다" << std::endl;
+	std::locale::global(std::locale("ko_KR.UTF-8"));
+	std::wcout.imbue(std::locale("ko_KR.UTF-8"));
+	std::cout.imbue(std::locale("ko_KR.UTF-8"));
 
-    Scene MainScene{};
+	std::cout << " 가나다라 " << std::endl;
 
-    Camera& MainCamera{ MainScene.GetMainCamera() };
-    MainCamera.SetClearColor(0.1F, 0.2F, 0.3F, 1.0F);
-    MainCamera.GetTransform().SetPosition(glm::vec3{ 0.0F, 3.0F, 8.0F });
-    MainCamera.GetTransform().SetRotation(glm::vec3{ -0.35F, 0.0F, 0.0F });
+	Scene MainScene{};
 
-    std::size_t GridIndex{ MainScene.CreatePrimitiveGameObject("Grid", PrimitiveMeshType::Grid) };
-    std::size_t CubeIndex{ MainScene.CreatePrimitiveGameObject("Cube", PrimitiveMeshType::Cube) };
-    std::size_t SphereIndex{ MainScene.CreatePrimitiveGameObject("Sphere", PrimitiveMeshType::Sphere) };
-    std::size_t TriangularPyramidIndex{ MainScene.CreatePrimitiveGameObject("TriangularPyramid", PrimitiveMeshType::TriangularPyramid) };
-    std::size_t SquarePyramidIndex{ MainScene.CreatePrimitiveGameObject("SquarePyramid", PrimitiveMeshType::SquarePyramid) };
+	Camera& MainCamera{ MainScene.GetMainCamera() };
+	MainCamera.SetClearColor(0.1F, 0.2F, 0.3F, 1.0F);
+	MainCamera.GetTransform().SetPosition(glm::vec3{ 0.0F, 3.0F, 8.0F });
+	MainCamera.GetTransform().SetRotation(glm::vec3{ -0.35F, 0.0F, 0.0F });
 
-    GameObject* GridObject{ MainScene.GetGameObject(GridIndex) };
-    if (GridObject != nullptr) {
-        GridObject->GetTransform().SetPosition(glm::vec3{ 0.0F, 0.0F, 0.0F });
-    }
+	std::size_t GridIndex{ MainScene.CreatePrimitiveGameObject("Grid", PrimitiveMeshType::Grid) };
+	std::size_t CubeIndex{ MainScene.CreatePrimitiveGameObject("Cube", PrimitiveMeshType::Cube) };
+	std::size_t SphereIndex{ MainScene.CreatePrimitiveGameObject("Sphere", PrimitiveMeshType::Sphere) };
+	std::size_t TriangularPyramidIndex{ MainScene.CreatePrimitiveGameObject("TriangularPyramid", PrimitiveMeshType::TriangularPyramid) };
+	std::size_t SquarePyramidIndex{ MainScene.CreatePrimitiveGameObject("SquarePyramid", PrimitiveMeshType::SquarePyramid) };
 
-    GameObject* CubeObject{ MainScene.GetGameObject(CubeIndex) };
-    if (CubeObject != nullptr) {
-        CubeObject->GetTransform().SetPosition(glm::vec3{ -2.0F, 0.5F, 0.0F });
-    }
+	GameObject* GridObject{ MainScene.GetGameObject(GridIndex) };
+	if (GridObject != nullptr) {
+		GridObject->GetTransform().SetPosition(glm::vec3{ 0.0F, 0.0F, 0.0F });
+	}
 
-    GameObject* SphereObject{ MainScene.GetGameObject(SphereIndex) };
-    if (SphereObject != nullptr) {
-        SphereObject->GetTransform().SetPosition(glm::vec3{ 2.0F, 0.5F, 0.0F });
-    }
+	GameObject* CubeObject{ MainScene.GetGameObject(CubeIndex) };
+	if (CubeObject != nullptr) {
+		CubeObject->GetTransform().SetPosition(glm::vec3{ -2.0F, 0.5F, 0.0F });
+	}
 
-    GameObject* TriangularPyramidObject{ MainScene.GetGameObject(TriangularPyramidIndex) };
-    if (TriangularPyramidObject != nullptr) {
-        TriangularPyramidObject->GetTransform().SetPosition(glm::vec3{ 0.0F, 0.5F, -2.0F });
-    }
+	GameObject* SphereObject{ MainScene.GetGameObject(SphereIndex) };
+	if (SphereObject != nullptr) {
+		SphereObject->GetTransform().SetPosition(glm::vec3{ 2.0F, 0.5F, 0.0F });
+	}
 
-    GameObject* SquarePyramidObject{ MainScene.GetGameObject(SquarePyramidIndex) };
-    if (SquarePyramidObject != nullptr) {
-        SquarePyramidObject->GetTransform().SetPosition(glm::vec3{ 0.0F, 0.5F, 2.0F });
-    }
+	GameObject* TriangularPyramidObject{ MainScene.GetGameObject(TriangularPyramidIndex) };
+	if (TriangularPyramidObject != nullptr) {
+		TriangularPyramidObject->GetTransform().SetPosition(glm::vec3{ 0.0F, 0.5F, -2.0F });
+	}
 
-    Renderer MainRenderer{};
+	GameObject* SquarePyramidObject{ MainScene.GetGameObject(SquarePyramidIndex) };
+	if (SquarePyramidObject != nullptr) {
+		SquarePyramidObject->GetTransform().SetPosition(glm::vec3{ 0.0F, 0.5F, 2.0F });
+	}
 
-    if (!MainRenderer.Initialize()) {
-        return -1;
-    }
+	Renderer MainRenderer{};
 
-    while (!MainRenderer.ShouldClose()) {
-        MainRenderer.ProcessInput(MainScene);
-        MainScene.Update();
-        MainRenderer.RenderFrame(MainScene);
-        MainRenderer.Present();
-        MainRenderer.PollEvents();
-    }
+	if (!MainRenderer.Initialize()) {
+		return -1;
+	}
 
-    MainRenderer.Shutdown();
+	while (!MainRenderer.ShouldClose()) {
+		MainRenderer.ProcessInput(MainScene);
+		MainScene.Update();
+		MainRenderer.RenderFrame(MainScene);
+		MainRenderer.Present();
+		MainRenderer.PollEvents();
+	}
 
-    return 0;
+	MainRenderer.Shutdown();
+
+	return 0;
 }
